ab8992f5d8f0e5595ff4d416123b07ba69a6ecb3
manavdesai27 <manav27202@gmail.com>
2023-02-17 16:18:31 +0530

    netlify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import SharedPlaylist from './components/SharedPlaylist';
 
-function App() {
+const firebaseConfig = {
+  // Your Firebase configuration goes here
+  apiKey: "AIzaSyB4mmmr5eSM7qD-bfeqK_AI0lu7GOYeSl4",
+  authDomain: "shared-playlist-c8f22.firebaseapp.com",
+  projectId: "shared-playlist-c8f22",
+  storageBucket: "shared-playlist-c8f22.appspot.com",
+  messagingSenderId: "1081323027610",
+  appId: "1:1081323027610:web:97adc161a5ae7729e37323",
+  measurementId: "G-VWLX9F33Q4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+    });
+  }, []);
+ 
+  const handleLogin = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+ 
+  const handleLogout = () => {
+    auth.signOut();
+  };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {currentUser ? (
+        <div>
+          <SharedPlaylist currentUser={currentUser} />
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <button onClick={handleLogin}>Login with Google</button>
+      )}
     </div>
   );
-}
-
-export default App;
+      }
+      export default App; 
---
+++ src/components/SharedPlaylist.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import Video from 'twilio-video';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const SharedPlaylist = ({ currentUser }) => {
+  const [room, setRoom] = useState(null);
+  const [participant, setParticipant] = useState(null);
+  const [playlist, setPlaylist] = useState([]);
+
+  const db = firebase.firestore();
+
+  useEffect(() => {
+    const roomName = 'cool room';
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzk2ZmExOTQ1N2ZkZTQ1YTkzOGNiY2M3MWZhODVkYTg0LTE2NzY2Mjc1MzkiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJ1c2VyIiwidmlkZW8iOnsicm9vbSI6ImNvb2wgcm9vbSJ9fSwiaWF0IjoxNjc2NjI3NTM5LCJleHAiOjE2NzY2MzExMzksImlzcyI6IlNLOTZmYTE5NDU3ZmRlNDVhOTM4Y2JjYzcxZmE4NWRhODQiLCJzdWIiOiJBQ2Y5ZDc5MDZlMzM2NjA4YzUyNjEwZDI5ZWE4ZWJkNTc2In0.-zNdfHDK-UXCQE4MChRd_4fhApSgZte_VjmTpHpYp9Y';
+
+    Video.connect(token, {
+      name: roomName
+    }).then(room => {
+      setRoom(room);
+      room.on('participantConnected', participant => {
+        setParticipant(participant);
+      });
+    });
+
+    return () => {
+      if (room) {
+        room.disconnect();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    // Load the playlist from Firebase
+    db.collection('playlists').doc('my-playlist').get().then(doc => {
+      if (doc.exists) {
+        setPlaylist(doc.data().songs);
+      }
+    });
+  }, [db]);
+
+  useEffect(() => {
+    // Play the current song when it changes
+    if (playlist.length > 0) {
+      const currentSong = playlist[0];
+      console.log(currentSong);
+      const audio = new Audio(currentSong.url);
+      console.log(audio);
+      audio.play();
+
+      // Synchronize playback with other participants
+      if (participant) {
+        const track = new Video.LocalDataTrack();
+        participant.publishTrack(track);
+
+        let lastPosition = 0;
+        setInterval(() => {
+          const position = audio.currentTime || 0;
+          if (position !== lastPosition) {
+            track.send({ position });
+            lastPosition = position;
+          }
+        }, 100);
+
+        room.on('trackPublished', publication => {
+          if (publication.track === track) {
+            publication.track.on('message', data => {
+              const { position } = JSON.parse(data);
+              if (position !== lastPosition) {
+                audio.currentTime = position;
+                lastPosition = position;
+              }
+            });
+          }
+        });
+      }
+    }
+  }, [participant, playlist]);
+
+  return (
+    <div>
+      <h1>Shared Playlist</h1>
+      <ul>
+        {playlist.map(song => (
+          <li key={song.url}>{song.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SharedPlaylist;
